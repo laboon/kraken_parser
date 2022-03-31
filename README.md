cdc5a5c71a7ef29b08b4de1c0122589bf41251fa
laboon <laboon@gmail.com>
2022-03-31 15:27:39 +0200

    updates

--- README.md
+++ README.md
@@ -3,23 +3,32 @@ For parsing Kraken staking data
 
 ## Instructions
 
-Download "ledger" data from Kraken. You can get it from the "History - Export" tab. This will give you a CSV file. 
+Download "ledger" data from Kraken. You can get it from the "History - Export" tab. This will give you a CSV file.
 
-### Calculating DOT Rewards
+Note that currently this calculates USD "staking" differently than token staking. You need to use usd_parser.rb, and not parser.rb, for this. See below for details.
 
-Run "ruby dot_parser.rb *name of file*"
+### Calculating DOT and KSM Rewards
 
-It will show you all DOT staking rewards received and their value. It will also show total DOT received and total USD value.
+Run "ruby parser.rb *name of file* *currency*"
 
-### Calculating KSM Rewards
+It will show you all DOT and KSM staking rewards received and their value. It will also show total DOT and KSM received and total value in USD, EUR, or CHF.
 
-Run "ruby ksm_parser.rb *name of file*"
+#### Example
 
-It will show you all KSM staking rewards received and their value. It will also show total KSM received and total USD value.
+```
+$ ruby parser.rb ../staking-rewards-collector/kraken_data.csv eur
+
+2021-12-24: 3.70716535 DOT ( 95.72 eur )
+2021-12-28: 5.04562268 DOT ( 138.25 eur )
+2021-12-31: 3.82805825 DOT ( 93.1 eur )
+No KSM rewards found.
+Total DOT Received: 12.58084628 ( $327.07 EUR )
+```
+
 
 ### Calculating USD Rewards
 
-Run "ruby usd_parser *name of file*"
+Run "ruby usd_parser.rb *name of file*"
 
 It will show you all USD staking rewards received and their value. It will also show total USD.M received and total USD value (which are the same, since we assume that the USD.M are equal to USD).
 
@@ -31,4 +40,3 @@ DOT prices were downloaded from CoinGecko and cover up until 31 Mar 2022. You ca
 
 1. Combine code into a single file and show all staking rewards.
 2. Support other tokens (should be relatively straightforward).
-3. Support other currencies besides USD.
---
+++ parser.rb
@@ -0,0 +1,162 @@
+require 'date'
+
+############################################################################
+# CONSTANTS AND GLOBAL VARIABLES
+############################################################################
+
+
+# Supported fiat currencies
+SUPPORTED_CURRENCIES = ['usd', 'chf', 'eur']
+# Currency used (default = usd)
+$currency = 'usd'
+
+# Supported tokens
+SUPPORTED_TOKENS = ['KSM', 'DOT']
+
+# Supported "staking" versions of tokens according to Kraken
+SUPPORTED_TOKENS_S = SUPPORTED_TOKENS.map { |x| x + ".S" }
+
+
+############################################################################
+# HELPER METHODS 
+############################################################################
+
+# Checks that valid arguments were passed in
+
+def check_args(args)
+  if args.count < 1
+    puts "You need to enter a filename."
+    exit 1
+  end
+
+  if args.count == 1
+    puts "Defaulting currency to USD."
+  end
+
+  if args.count > 2
+    puts "Too many arguments! Enter only FILENAME and CURRENCY."
+    puts "Supported currencies: #{SUPPORTED_CURRENCIES}"
+    exit 1
+  end
+
+  if args.count == 2
+    if !SUPPORTED_CURRENCIES.include?(args[1].downcase)
+      puts "#{args[1]} is not a supported currency."
+      puts "Supported currencies: #{SUPPORTED_CURRENCIES}"
+      exit 1
+    end
+  end
+  
+end
+
+
+def print_totals(total_tokens, total_value)
+  SUPPORTED_TOKENS.each do |kind|
+    if total_tokens[kind] > 0
+      print "Total #{kind} Received: #{total_tokens[kind]} "
+      puts "( $#{total_value[kind].round(2)} #{$currency.upcase} )"
+    else
+      puts "No #{kind} rewards found."
+    end
+  end
+
+end
+
+
+############################################################################
+# EXECUTION STARTS HERE
+############################################################################
+
+check_args(ARGV)
+
+file_loc = ARGV[0]
+$currency = ARGV[1]
+
+all_prices = {}
+
+SUPPORTED_TOKENS.each do |token|
+  stake_kind = token + ".S"
+  price_file = "./data/#{token.downcase}-#{$currency}.csv"
+  prices = {}
+
+  # Load in pricing information
+
+  first_line = true
+
+  File.foreach(price_file) do |line|
+    
+    # Ignore first line
+    if first_line
+      first_line = false
+      next
+    end
+
+    data = line.split(",").map { |l| l.delete_prefix('"').delete_suffix('"') }
+    
+    date = Date.parse(data[0])
+    price = data[1]
+    prices[date] = price
+    
+    
+  end
+
+  all_prices[token] = prices
+
+  
+end
+
+
+# Load in staking data
+
+staking_data = []
+
+File.foreach(file_loc) do |line|
+  data = line.split(",").map { |l| l.delete_prefix('"').delete_suffix('"') }
+  deposit_or_staking = data[3]
+  kind = data[6]
+  if SUPPORTED_TOKENS_S.include?(kind) && deposit_or_staking == "staking"
+    staking_event = []
+    staking_event << Date.parse(data[2]) << data[7] << kind
+    staking_data << staking_event
+  end
+  
+end
+
+if staking_data.count.zero?
+  puts "No staking events found in this CSV file."
+  exit 1
+end
+
+
+
+
+# pre-populate token amounts and values
+
+
+total_tokens = {}
+total_value = {}
+
+SUPPORTED_TOKENS.each do |kind|
+  total_tokens[kind] = 0
+  total_value[kind] = 0
+end
+
+
+# calculate staking data
+
+
+staking_data.each do |event|
+  date = event[0] # get date
+  amount = event[1].to_f # get amount of rewards in token
+  kind = event[2].chop.chop # remove .S to get raw token name
+  price = all_prices[kind][date].to_f.round(2)
+  value = (amount * price).round(2)
+  total_tokens[kind] += amount
+  total_value[kind] += value
+  puts "#{date}: #{amount} #{kind} ( #{value} #{$currency.upcase} )"
+  
+end
+
+print_totals(total_tokens, total_value)
+
+
